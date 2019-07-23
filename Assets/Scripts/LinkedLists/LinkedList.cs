599cf5d9131e2a31088fd6c0e868e6f3d2254812
Joseph Leybovich <joseph.leybovich@gmail.com>
2019-07-23 02:47:30 -0700

    LinkedList Impl

---
+++ Assets/Scripts/LinkedLists/LinkedList.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace InterviewQuestions.LinkedLists
+{
+    public class LinkedList<T> : IEnumerable<T>
+    {
+        private class LinkedListEnumerator : IEnumerator<T>
+        {
+            public LinkedList<T> list;
+            LinkedListNode<T> position;
+
+            public LinkedListEnumerator(LinkedList<T> list)
+            {
+                this.list = list;
+            }
+
+            public bool MoveNext()
+            {
+                if (position == null)
+                {
+                    position = list.Head;
+                }
+                else
+                {
+                    position = position.Next;
+                }
+                
+                return position != null;
+            }
+
+            public void Reset()
+            {
+                position = null;
+            }
+
+            public void Dispose()
+            {
+                position = null;
+            }
+
+            public T Current
+            {
+                get
+                {
+                    return position.Value;
+                }
+            }
+
+            object IEnumerator.Current => Current;
+        }
+
+        public LinkedListNode<T> Head { get; protected set; }
+        public LinkedListNode<T> Tail { get; protected set; }
+
+        public int Count { get; protected set; }
+
+        public LinkedList(params T[] values)
+        {
+            Add(values);
+        }
+
+        public T this[int key]
+        {
+            get
+            {
+                if(key < 0 || key >= Count)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+                var current = Head;
+
+                for (int i = 0; i < key; ++i)
+                {
+                    if(current == null)
+                    {
+                        throw new IndexOutOfRangeException();
+                    }
+
+                    current = current.Next;
+                }
+
+                return current.Value;
+            }
+            set
+            {
+                if (key < 0 || key >= Count)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+
+                var current = Head;
+                
+                for (int i = 0; i < key; ++i)
+                {
+                    current = current.Next;
+                }
+
+                current.Value = value;
+            }
+        }
+
+        public void Add(T value)
+        {
+            var newNode = new LinkedListNode<T>(value);
+
+            if(Head == null)
+            {
+                Head = newNode;
+            }
+            else
+            {
+                Tail.Next = newNode;
+            }
+
+            Tail = newNode;
+            ++Count;
+        }
+
+        public void Add(params T[] values)
+        {
+            foreach(var value in values)
+            {
+                Add(value);
+            }
+        }
+
+        public void Remove(T value)
+        {
+            if (Head == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            LinkedListNode<T> previous = null;
+            var current = Head;
+
+            while(current != null)
+            {
+                if(current.Value.Equals(value))
+                {
+                    break;
+                }
+
+                previous = current;
+                current = current.Next;
+            }
+
+            if(current == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            if(previous != null)
+            {
+                previous.Next = current.Next;
+            }
+            else
+            {
+                Head = current.Next;
+            }
+
+            if(current.Next == null)
+            {
+                Tail = previous;
+            }
+
+            --Count;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= Count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            LinkedListNode<T> previous = null;
+            var current = Head;
+
+            for (int i = 0; i < index; ++i)
+            {
+                previous = current;
+                current = current.Next;
+            }
+
+            if (previous != null)
+            {
+                previous.Next = current.Next;
+            }
+            else
+            {
+                Head = current.Next;
+            }
+
+            if (current.Next == null)
+            {
+                Tail = previous;
+            }
+
+            --Count;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new LinkedListEnumerator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Assets/Scripts/Tests/LinkedLists/LinkedListTests.cs
@@ -0,0 +1,190 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace InterviewQuestions.LinkedLists.Tests
+{
+    public class LinkedListTests
+    {
+        private LinkedList<int> CreateList()
+        {
+            return new LinkedList<int>(0, 1, 2);
+        }
+
+        [Test]
+        public void Constructor_NullArray_DoesNotThrow()
+        {
+            Assert.DoesNotThrow(() => new LinkedList<int>());
+        }
+
+        [Test]
+        public void Constructor_ValidArray_AddsInOrder()
+        {
+            var actual = new LinkedList<int>(0, 1, 2);
+
+            for(var expected = 0; expected < 3; ++expected)
+            {
+                Assert.AreEqual(expected, actual[expected]);
+            }
+        }
+
+        [TestCase(-1)]
+        [TestCase(3)]
+        public void Subscript_InvalidIndex_ThrowsIndexOutOfRange(int index)
+        {
+            var list = CreateList();
+
+            int act()
+            {
+                return list[index];
+            }
+
+            Assert.Throws<IndexOutOfRangeException>(() => act());
+        }
+
+        [TestCase(0)]
+        [TestCase(1)]
+        [TestCase(2)]
+        public void Subscript_ValidIndex_ReturnsExpected(int index)
+        {
+            var list = CreateList();
+
+            var actual = list[index];
+
+            Assert.AreEqual(index, actual);
+        }
+
+        [TestCase(-1)]
+        [TestCase(3)]
+        public void SubscriptSet_InvalidIndex_ThrowsIndexOutOfRange(int index)
+        {
+            var list = CreateList();
+            Assert.Throws<IndexOutOfRangeException>(() => list[index] = 0);
+        }
+
+        [TestCase(new int[] { 0 })]
+        [TestCase(new int[] { 0, 1 })]
+        [TestCase(new int[] { 0, 1, 2 })]
+        public void Add_ValidInput_SetsHead(int[] input)
+        {
+            var list = new LinkedList<int>();
+            var expected = input[0];
+
+            foreach(var value in input)
+            {
+                list.Add(value);
+            }
+
+            Assert.AreEqual(expected, list.Head.Value);
+        }
+
+        [TestCase(new int[] { 0 })]
+        [TestCase(new int[] { 0, 1 })]
+        [TestCase(new int[] { 0, 1, 2 })]
+        public void Add_ValidInput_SetsTail(int[] input)
+        {
+            var list = new LinkedList<int>();
+
+            foreach (var value in input)
+            {
+                list.Add(value);
+                Assert.AreEqual(value, list.Tail.Value);
+            }
+        }
+
+        [TestCase(new int[] { 0 })]
+        [TestCase(new int[] { 0, 1 })]
+        [TestCase(new int[] { 0, 1, 2 })]
+        public void Add_ValidInput_IncrementCount(int[] input)
+        {
+            var list = new LinkedList<int>();
+
+            foreach (var value in input)
+            {
+                list.Add(value);
+                Assert.AreEqual(value + 1, list.Count);
+            }
+        }
+
+        [Test]
+        public void Remove_NullHead_ThrowsInvalidOperation()
+        {
+            var list = new LinkedList<int>();
+            Assert.Throws<InvalidOperationException>(() => list.Remove(0));
+        }
+
+        [Test]
+        public void Remove_Head_SetsHead()
+        {
+            var list = CreateList();
+
+            list.Remove(0);
+
+            Assert.AreEqual(1, list.Head.Value);
+        }
+
+        [Test]
+        public void Remove_Tail_SetsTail()
+        {
+            var list = CreateList();
+
+            list.Remove(2);
+
+            Assert.AreEqual(1, list.Tail.Value);
+        }
+
+        [TestCase(new int[] { 0 })]
+        [TestCase(new int[] { 0, 1 })]
+        [TestCase(new int[] { 0, 1, 2 })]
+        public void Remove_ValidInput_DecrementCount(int[] input)
+        {
+            var list = new LinkedList<int>(input);
+
+            foreach(var value in input)
+            {
+                list.Remove(value);
+                Assert.AreEqual(input.Length - 1 - value, list.Count);
+            }
+        }
+
+        [TestCase(-1)]
+        [TestCase(3)]
+        public void RemoveAt_InvalidIndex_ThrowsIndexOutOfRange(int index)
+        {
+            var list = CreateList();
+            Assert.Throws<IndexOutOfRangeException>(() => list.RemoveAt(index));
+        }
+
+        [Test]
+        public void RemoveAt_Head_SetsHead()
+        {
+            var list = CreateList();
+
+            list.RemoveAt(0);
+
+            Assert.AreEqual(1, list.Head.Value);
+        }
+
+        [Test]
+        public void RemoveAt_Tail_SetsTail()
+        {
+            var list = CreateList();
+
+            list.RemoveAt(2);
+
+            Assert.AreEqual(1, list.Tail.Value);
+        }
+
+        [Test]
+        public void Foreach_ValidList_ReturnsInOrder()
+        {
+            var list = CreateList();
+            var expected = 0;
+
+            foreach (var value in list)
+            {
+                Assert.AreEqual(expected, value);
+                expected++;
+            }
+        }
+    }
+}
