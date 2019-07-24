fa5d1a64c0aea6772f150e97b6c9e188b494422c
Joseph Leybovich <joseph.leybovich@gmail.com>
2019-07-24 01:09:05 -0700

    lists of depths

---
+++ Assets/Scripts/Tests/TreesAndGraphs/ListOfDepthsTests.cs
@@ -0,0 +1,86 @@
+﻿using NUnit.Framework;
+
+namespace InterviewQuestions.TreesAndGraphs.Tests
+{
+	public class ListOfDepthsTests
+	{
+        private TreeNode<int> CreateOneNode()
+		{
+			return new TreeNode<int>(0);
+		}
+
+        private TreeNode<int> CreateTwoLevels()
+        {
+            var root = new TreeNode<int>(0);
+            var left = new TreeNode<int>(1);
+            var right = new TreeNode<int>(2);
+            root.Left = left;
+            root.Right = right;
+
+            return root;
+        }
+
+        private TreeNode<int> CreateThreeLevels()
+        {
+            var root = new TreeNode<int>(0);
+            var left = new TreeNode<int>(1);
+            var right = new TreeNode<int>(2);
+            var left2 = new TreeNode<int>(3);
+            var right2 = new TreeNode<int>(4);
+
+            root.Left = left;
+            root.Right = right;
+
+            left.Right = right2;
+            right.Left = left2;
+
+            return root;
+        }
+
+        [Test]
+        public void ListOfDepths_Null_Null()
+		{
+			var actual = TreeExtensions.CreateListOfDepths<int>(null);
+			Assert.AreEqual(null, actual);
+		}
+
+		[Test]
+		public void ListOfDepths_OneNode_One()
+		{
+			var tree = CreateOneNode();
+
+			var actual = TreeExtensions.CreateListOfDepths(tree);
+
+			Assert.AreEqual(1, actual.Count);
+			Assert.AreEqual(0, actual[0].First.Value.Value);
+		}
+
+        [Test]
+        public void ListOfDepths_TwoLevels_ExpectedResult()
+        {
+            var tree = CreateTwoLevels();
+
+            var actual = TreeExtensions.CreateListOfDepths(tree);
+
+            Assert.AreEqual(2, actual.Count);
+            Assert.AreEqual(0, actual[0].First.Value.Value);
+            Assert.AreEqual(1, actual[1].First.Value.Value);
+            Assert.AreEqual(2, actual[1].First.Next.Value.Value);
+        }
+
+        [Test]
+        public void ListOfDepths_ThreeLevels_ExpectedResult()
+        {
+            var tree = CreateThreeLevels();
+
+            var actual = TreeExtensions.CreateListOfDepths(tree);
+
+            Assert.AreEqual(3, actual.Count);
+            Assert.AreEqual(0, actual[0].First.Value.Value);
+            Assert.AreEqual(1, actual[1].First.Value.Value);
+            Assert.AreEqual(2, actual[1].First.Next.Value.Value);
+            Assert.AreEqual(4, actual[2].First.Value.Value);
+            Assert.AreEqual(3, actual[2].First.Next.Value.Value);
+        }
+    }
+}
---
+++ Assets/Scripts/TreesAndGraphs/ListOfDepths.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewQuestions.TreesAndGraphs
+{
+    public static partial class TreeExtensions
+    {
+        public static List<LinkedList<TreeNode<T>>> CreateListOfDepths<T>(TreeNode<T> root)
+        {
+            if (root == null) return null;
+
+            var result = new List<LinkedList<TreeNode<T>>>();
+
+            var currentList = new LinkedList<TreeNode<T>>();
+            currentList.AddLast(root);
+
+            while(currentList.Count > 0)
+            {
+                result.Add(currentList);
+
+                var parents = currentList;
+                currentList = new LinkedList<TreeNode<T>>();
+
+                foreach(var parent in parents)
+                {
+                    if(parent.Left != null)
+                    {
+                        currentList.AddLast(parent.Left);
+                    }
+
+                    if (parent.Right != null)
+                    {
+                        currentList.AddLast(parent.Right);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
