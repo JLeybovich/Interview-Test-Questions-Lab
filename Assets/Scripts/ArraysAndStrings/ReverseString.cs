d9cc25aa645cec272e9c1475e411079bff10a556
Joseph Leybovich <joseph.leybovich@gmail.com>
2020-04-15 20:41:17 -0700

    reverse string in place

---
+++ Assets/Scripts/ArraysAndStrings/ReverseString.cs
@@ -0,0 +1,21 @@
+﻿namespace InterviewQuestions.ArraysAndStrings
+{
+    public static partial class StringExtensions
+    {
+        public static void Reverse(char[] arrayOfChars)
+        {
+            // Reverse input array of characters in place
+            // 1. Start with indices at beginning and end
+            // 2. Loop till we reach the middle
+            // 3. Swap the characters at the indices
+
+            for (int headIdx = 0, tailIdx = arrayOfChars.Length - 1;
+                headIdx < tailIdx; ++headIdx, --tailIdx)
+            {
+                arrayOfChars[headIdx] ^= arrayOfChars[tailIdx];
+                arrayOfChars[tailIdx] ^= arrayOfChars[headIdx];
+                arrayOfChars[headIdx] ^= arrayOfChars[tailIdx];
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Tests/ArraysAndStrings/ReverseStringTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace InterviewQuestions.ArraysAndStrings.Tests
+{
+    public class ReverseStringTests
+    {
+        [Test]
+        public void EmptyStringTest()
+        {
+            var expected = "".ToCharArray();
+            var actual = "".ToCharArray();
+            StringExtensions.Reverse(actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SingleCharacterStringTest()
+        {
+            var expected = "A".ToCharArray();
+            var actual = "A".ToCharArray();
+            StringExtensions.Reverse(actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void LongerStringTest()
+        {
+            var expected = "EDCBA".ToCharArray();
+            var actual = "ABCDE".ToCharArray();
+            StringExtensions.Reverse(actual);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
