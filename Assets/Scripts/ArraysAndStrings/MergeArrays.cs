50b23e12acd938d27e6a12f9081823454589b90a
Joseph Leybovich <joseph.leybovich@gmail.com>
2020-04-16 21:00:07 -0700

    merege arrays

---
+++ Assets/Scripts/ArraysAndStrings/MergeArrays.cs
@@ -0,0 +1,92 @@
+﻿namespace InterviewQuestions.ArraysAndStrings
+{
+    public static partial class ArrayExtensions
+    {
+        public static int[] MergeArraysMine(int[] myArray, int[] alicesArray)
+        {
+            // Combine the sorted arrays into one large sorted array
+            // 1. Keep Indices of both
+            // 2. While Indices are less the associated array lengths
+            // 3. Compare values at indices
+            // 4. Add whichever is less
+            // 5. Increment the associatted index 
+            // 6. Continue iteration
+            // 7. Add any remaining elements for either array
+
+            int[] mergedArray = new int[myArray.Length + alicesArray.Length];
+            int myIndex = 0, alicesIndex = 0, mergedIndex = 0;
+            while (myIndex < myArray.Length && alicesIndex < alicesArray.Length)
+            {
+                int myValue = myArray[myIndex];
+                int alicesValue = alicesArray[alicesIndex];
+
+                if (myValue < alicesValue)
+                {
+                    mergedArray[mergedIndex] = myValue;
+                    ++myIndex;
+                }
+                else
+                {
+                    mergedArray[mergedIndex] = alicesValue;
+                    ++alicesIndex;
+                }
+
+                ++mergedIndex;
+            }
+
+            while (myIndex < myArray.Length)
+            {
+                mergedArray[mergedIndex] = myArray[myIndex];
+                ++myIndex;
+                ++mergedIndex;
+            }
+
+            while (alicesIndex < alicesArray.Length)
+            {
+                mergedArray[mergedIndex] = alicesArray[alicesIndex];
+                ++alicesIndex;
+                ++mergedIndex;
+            }
+
+            return mergedArray;
+        }
+
+        public static int[] MergeArrays(int[] myArray, int[] alicesArray)
+        {
+            // Set up our mergedArray
+            var mergedArray = new int[myArray.Length + alicesArray.Length];
+
+            int currentIndexAlices = 0;
+            int currentIndexMine = 0;
+            int currentIndexMerged = 0;
+
+            while (currentIndexMerged < mergedArray.Length)
+            {
+                bool isMyArrayExhausted = currentIndexMine >= myArray.Length;
+                bool isAlicesArrayExhausted = currentIndexAlices >= alicesArray.Length;
+
+                // Case: next comes from my array
+                // My array must not be exhausted, and EITHER:
+                // 1) Alice's array IS exhausted, or
+                // 2) the current element in my array is less
+                //    than the current element in Alice's array
+                if (!isMyArrayExhausted && (isAlicesArrayExhausted
+                        || (myArray[currentIndexMine] < alicesArray[currentIndexAlices])))
+                {
+                    mergedArray[currentIndexMerged] = myArray[currentIndexMine];
+                    currentIndexMine++;
+                }
+                else
+                {
+                    // Case: next comes from Alice's array
+                    mergedArray[currentIndexMerged] = alicesArray[currentIndexAlices];
+                    currentIndexAlices++;
+                }
+
+                currentIndexMerged++;
+            }
+
+            return mergedArray;
+        }
+    }
+}
---
+++ Assets/Scripts/Tests/ArraysAndStrings/MergeArraysTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using NUnit.Framework;
+
+namespace InterviewQuestions.ArraysAndStrings.Tests
+{
+    public class MergeArraysTests
+    {
+        [Test]
+        public void BothArraysAreEmptyTest()
+        {
+            var myArray = new int[] { };
+            var alicesArray = new int[] { };
+            var expected = new int[] { };
+            var actual = ArrayExtensions.MergeArrays(myArray, alicesArray);
+            Assert.AreEqual(expected, actual);
+            actual = ArrayExtensions.MergeArraysMine(myArray, alicesArray);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void FirstArrayIsEmptyTest()
+        {
+            var myArray = new int[] { };
+            var alicesArray = new int[] { 1, 2, 3 };
+            var expected = new int[] { 1, 2, 3 };
+            var actual = ArrayExtensions.MergeArrays(myArray, alicesArray);
+            Assert.AreEqual(expected, actual);
+            actual = ArrayExtensions.MergeArraysMine(myArray, alicesArray);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SecondArrayIsEmptyTest()
+        {
+            var myArray = new int[] { 5, 6, 7 };
+            var alicesArray = new int[] { };
+            var expected = new int[] { 5, 6, 7 };
+            var actual = ArrayExtensions.MergeArrays(myArray, alicesArray);
+            Assert.AreEqual(expected, actual);
+            actual = ArrayExtensions.MergeArraysMine(myArray, alicesArray);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void BothArraysHaveSomeNumbersTest()
+        {
+            var myArray = new int[] { 2, 4, 6 };
+            var alicesArray = new int[] { 1, 3, 7 };
+            var expected = new int[] { 1, 2, 3, 4, 6, 7 };
+            var actual = ArrayExtensions.MergeArrays(myArray, alicesArray);
+            Assert.AreEqual(expected, actual);
+            actual = ArrayExtensions.MergeArraysMine(myArray, alicesArray);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ArraysAreDifferentLengthsTest()
+        {
+            var myArray = new int[] { 2, 4, 6, 8 };
+            var alicesArray = new int[] { 1, 7 };
+            var expected = new int[] { 1, 2, 4, 6, 7, 8 };
+            var actual = ArrayExtensions.MergeArrays(myArray, alicesArray);
+            Assert.AreEqual(expected, actual);
+            actual = ArrayExtensions.MergeArraysMine(myArray, alicesArray);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
