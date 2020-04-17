d05231376ea7dca701adcd766e9e03b593bd18be
Joseph Leybovich <joseph.leybovich@gmail.com>
2020-04-16 22:38:26 -0700

    order checker

---
+++ Assets/Scripts/ArraysAndStrings/OrderChecker.cs
@@ -0,0 +1,46 @@
+﻿namespace InterviewQuestions.ArraysAndStrings
+{
+    public static partial class ArrayExtensions
+    {
+        public static bool IsFirstComeFirstServed(int[] takeOutOrders, int[] dineInOrders, int[] servedOrders)
+        {
+            int takeOutOrdersIndex = 0;
+            int dineInOrdersIndex = 0;
+
+            foreach (var order in servedOrders)
+            {
+                if (takeOutOrdersIndex < takeOutOrders.Length && order == takeOutOrders[takeOutOrdersIndex])
+                {
+                    // If we still have orders in takeOutOrders
+                    // and the current order in takeOutOrders is the same
+                    // as the current order in servedOrders
+                    takeOutOrdersIndex++;
+                }
+                else if (dineInOrdersIndex < dineInOrders.Length && order == dineInOrders[dineInOrdersIndex])
+                {
+                    // If we still have orders in dineInOrders
+                    // and the current order in dineInOrders is the same
+                    // as the current order in servedOrders
+                    dineInOrdersIndex++;
+                }
+                else
+                {
+                    // If the current order in servedOrders doesn't match the current
+                    // order in takeOutOrders or dineInOrders, then we're not serving first-come,
+                    // first-served
+                    return false;
+                }
+            }
+
+            // Check for any extra orders at the end of takeOutOrders or dineInOrders
+            if (dineInOrdersIndex != dineInOrders.Length || takeOutOrdersIndex != takeOutOrders.Length)
+            {
+                return false;
+            }
+
+            // All orders in servedOrders have been "accounted for"
+            // so we're serving first-come, first-served!
+            return true;
+        }
+    }
+}
---
+++ Assets/Scripts/Tests/ArraysAndStrings/OrderCheckerTests.cs
@@ -0,0 +1,77 @@
+﻿using NUnit.Framework;
+
+namespace InterviewQuestions.ArraysAndStrings.Tests
+{
+    public class OrderCheckerTests
+    {
+        [Test]
+        public void BothRegistersHaveSameNumberOfOrdersTest()
+        {
+            var takeOutOrders = new int[] { 1, 4, 5 };
+            var dineInOrders = new int[] { 2, 3, 6 };
+            var servedOrders = new int[] { 1, 2, 3, 4, 5, 6 };
+            var result = ArrayExtensions.IsFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders);
+            Assert.True(result);
+        }
+
+        [Test]
+        public void RegistersHaveDifferentLengthsTest()
+        {
+            var takeOutOrders = new int[] { 1, 5 };
+            var dineInOrders = new int[] { 2, 3, 6 };
+            var servedOrders = new int[] { 1, 2, 6, 3, 5 };
+            var result = ArrayExtensions.IsFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders);
+            Assert.False(result);
+        }
+
+        [Test]
+        public void OneRegisterIsEmptyTest()
+        {
+            var takeOutOrders = new int[] { };
+            var dineInOrders = new int[] { 2, 3, 6 };
+            var servedOrders = new int[] { 2, 3, 6 };
+            var result = ArrayExtensions.IsFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders);
+            Assert.True(result);
+        }
+
+        [Test]
+        public void ServedOrdersIsMissingOrdersTest()
+        {
+            var takeOutOrders = new int[] { 1, 5 };
+            var dineInOrders = new int[] { 2, 3, 6 };
+            var servedOrders = new int[] { 1, 6, 3, 5 };
+            var result = ArrayExtensions.IsFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders);
+            Assert.False(result);
+        }
+
+        [Test]
+        public void ServedOrdersHasExtraOrders()
+        {
+            var takeOutOrders = new int[] { 1, 5 };
+            var dineInOrders = new int[] { 2, 3, 6 };
+            var servedOrders = new int[] { 1, 2, 3, 5, 6, 8 };
+            var result = ArrayExtensions.IsFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders);
+            Assert.False(result);
+        }
+
+        [Test]
+        public void OneRegisterHasExtraOrders()
+        {
+            var takeOutOrders = new int[] { 1, 9 };
+            var dineInOrders = new int[] { 7, 8 };
+            var servedOrders = new int[] { 1, 7, 8 };
+            var result = ArrayExtensions.IsFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders);
+            Assert.False(result);
+        }
+
+        [Test]
+        public void OneRegisterHasUnservedOrders()
+        {
+            var takeOutOrders = new int[] { 55, 9 };
+            var dineInOrders = new int[] { 7, 8 };
+            var servedOrders = new int[] { 1, 7, 8, 9 };
+            var result = ArrayExtensions.IsFirstComeFirstServed(takeOutOrders, dineInOrders, servedOrders);
+            Assert.False(result);
+        }
+    }
+}
