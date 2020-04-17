ad9f407499c82392d4d8bb2d3ce9efb5f6c86683
Joseph Leybovich <joseph.leybovich@gmail.com>
2020-04-16 20:43:36 -0700

    reverse words

---
+++ Assets/Scripts/ArraysAndStrings/ReverseWords.cs
@@ -0,0 +1,38 @@
+﻿namespace InterviewQuestions.ArraysAndStrings
+{
+    public static partial class StringExtensions
+    {
+        public static void ReverseWords(char[] message)
+        {
+            // Decode the message by reversing the words
+
+            ReverseChars(message, 0, message.Length - 1);
+
+            int currentWordStartIndex = 0;
+            for (int i = 0; i <= message.Length; i++)
+            {
+                // Found the end of the current word!
+                if (i == message.Length || message[i] == ' ')
+                {
+                    // If we haven't exhausted the array our
+                    // next word's start is one character ahead
+                    ReverseChars(message, currentWordStartIndex, i - 1);
+                    currentWordStartIndex = i + 1;
+                }
+            }
+        }
+
+        public static void ReverseChars(char[] arrayOfChars, int headIdx, int tailIdx)
+        {
+            while (headIdx < tailIdx)
+            {
+                arrayOfChars[headIdx] ^= arrayOfChars[tailIdx];
+                arrayOfChars[tailIdx] ^= arrayOfChars[headIdx];
+                arrayOfChars[headIdx] ^= arrayOfChars[tailIdx];
+
+                ++headIdx;
+                --tailIdx;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Tests/ArraysAndStrings/ReverseWordsTests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+
+namespace InterviewQuestions.ArraysAndStrings.Tests
+{
+    public class ReverseWordsTests
+    {
+        [Test]
+        public void OneWordTest()
+        {
+            var expected = "vault".ToCharArray();
+            var actual = "vault".ToCharArray();
+            StringExtensions.ReverseWords(actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TwoWordsTest()
+        {
+            var expected = "cake thief".ToCharArray();
+            var actual = "thief cake".ToCharArray();
+            StringExtensions.ReverseWords(actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ThreeWordsTest()
+        {
+            var expected = "get another one".ToCharArray();
+            var actual = "one another get".ToCharArray();
+            StringExtensions.ReverseWords(actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void MultipleWordsSameLengthTest()
+        {
+            var expected = "the cat ate the rat".ToCharArray();
+            var actual = "rat the ate cat the".ToCharArray();
+            StringExtensions.ReverseWords(actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void MultipleWordsDifferentLengthsTest()
+        {
+            var expected = "chocolate bundt cake is yummy".ToCharArray();
+            var actual = "yummy is cake bundt chocolate".ToCharArray();
+            StringExtensions.ReverseWords(actual);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void EmptyStringTest()
+        {
+            var expected = "".ToCharArray();
+            var actual = "".ToCharArray();
+            StringExtensions.ReverseWords(actual);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
