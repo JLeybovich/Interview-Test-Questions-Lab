0b8796d075669088777976d9c9f3076ac526a4cd
Joseph Leybovich <joseph.leybovich@gmail.com>
2020-04-18 23:27:26 -0700

    Unity 2019.3.10f1, inflight entertainment

---
+++ Assets/Scripts/Tests/HashTables/InflightEntertainmentTests.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+
+namespace InterviewQuestions.HashTables
+{
+    public class InflightEntertainmentTests
+    {
+        [Test]
+        public void ShortFlightTest()
+        {
+            var result = HashTableExtensions.CanTwoMoviesFillFlight(new int[] { 2, 4 }, 1);
+            Assert.False(result);
+        }
+
+        [Test]
+        public void LongFlightTest()
+        {
+            var result = HashTableExtensions.CanTwoMoviesFillFlight(new int[] { 2, 4 }, 6);
+            Assert.True(result);
+        }
+
+        [Test]
+        public void OnlyOneMovieHalfFlightLenghtTest()
+        {
+            var result = HashTableExtensions.CanTwoMoviesFillFlight(new int[] { 3, 8 }, 6);
+            Assert.False(result);
+        }
+
+        [Test]
+        public void TwoMoviesHalfFlightLengthTest()
+        {
+            var result = HashTableExtensions.CanTwoMoviesFillFlight(new int[] { 3, 8, 3 }, 6);
+            Assert.True(result);
+        }
+
+        [Test]
+        public void LotsOfPossiblePairsTest()
+        {
+            var result = HashTableExtensions.CanTwoMoviesFillFlight(new int[] { 1, 2, 3, 4, 5, 6 }, 7);
+            Assert.True(result);
+        }
+
+        [Test]
+        public void NotUsingFirstMovieTest()
+        {
+            var result = HashTableExtensions.CanTwoMoviesFillFlight(new int[] { 4, 3, 2 }, 5);
+            Assert.True(result);
+        }
+
+        [Test]
+        public void OneMovieTest()
+        {
+            var result = HashTableExtensions.CanTwoMoviesFillFlight(new int[] { 6 }, 6);
+            Assert.False(result);
+        }
+
+        [Test]
+        public void NoMoviesTest()
+        {
+            var result = HashTableExtensions.CanTwoMoviesFillFlight(new int[] { }, 6);
+            Assert.False(result);
+        }
+    }
+}
