f70d6d3920b9bdedc3079db7624a59d304a940c1
Joseph Leybovich <joseph.leybovich@gmail.com>
2020-05-05 22:30:26 -0700

    top scores

---
+++ Assets/Scripts/HashTables/TopScores.cs
@@ -0,0 +1,37 @@
+﻿namespace InterviewQuestions.HashTables
+{
+    public static partial class HashTableExtensions
+    {
+        public static int[] SortScores(int[] unorderedScores, int highestPossibleScore)
+        {
+            // Array of 0s at indices 0..highestPossibleScore
+            int[] scoreCounts = new int[highestPossibleScore + 1];
+
+            // Populate scoreCounts
+            foreach (var score in unorderedScores)
+            {
+                scoreCounts[score]++;
+            }
+
+            // Populate the final sorted array
+            int[] sortedScores = new int[unorderedScores.Length];
+            int currentSortedIndex = 0;
+
+            // For each item in scoreCounts
+            for (int score = highestPossibleScore; score >= 0; score--)
+            {
+                int count = scoreCounts[score];
+
+                // For the number of times the item occurs
+                for (int occurrence = 0; occurrence < count; occurrence++)
+                {
+                    // Add it to the sorted array
+                    sortedScores[currentSortedIndex] = score;
+                    currentSortedIndex++;
+                }
+            }
+
+            return sortedScores;
+        }
+    }
+}
---
+++ Assets/Scripts/Tests/HashTables/TopScoresTests.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+
+namespace InterviewQuestions.HashTables
+{
+    public class TopScoresTests
+    {
+        [Test]
+        public void NoScoresTest()
+        {
+            var scores = new int[] { };
+            var expected = new int[] { };
+            var actual = HashTableExtensions.SortScores(scores, 100);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void OneScoreTest()
+        {
+            var scores = new int[] { 55 };
+            var expected = new int[] { 55 };
+            var actual = HashTableExtensions.SortScores(scores, 100);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TwoScoresTest()
+        {
+            var scores = new int[] { 30, 60 };
+            var expected = new int[] { 60, 30 };
+            var actual = HashTableExtensions.SortScores(scores, 100);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ManyScoresTest()
+        {
+            var scores = new int[] { 37, 89, 41, 65, 91, 53 };
+            var expected = new int[] { 91, 89, 65, 53, 41, 37 };
+            var actual = HashTableExtensions.SortScores(scores, 100);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void RepeatedScoresTest()
+        {
+            var scores = new int[] { 20, 10, 30, 30, 10, 20 };
+            var expected = new int[] { 30, 30, 20, 20, 10, 10 };
+            var actual = HashTableExtensions.SortScores(scores, 100);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
