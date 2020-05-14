419c6a46321ce568df1e9acfc463b3c279428589
Joseph Leybovich <joseph.leybovich@gmail.com>
2020-05-14 15:07:55 -0700

    find dups

---
+++ Assets/Scripts/ArraysAndStrings/FindDups.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+
+namespace InterviewQuestions.ArraysAndStrings
+{
+    public static partial class ArrayExtensions
+    {
+        public static int FindRepeat(int[] numbers)
+        {
+            int floor = 1;
+            int ceiling = numbers.Length - 1;
+
+            while (floor < ceiling)
+            {
+                // Divide our range 1..n into an upper range and lower range
+                // (such that they don't overlap)
+                // Lower range is floor..midpoint
+                // Upper range is midpoint+1..ceiling
+                int midpoint = floor + (ceiling - floor) / 2;
+                int lowerRangeFloor = floor;
+                int lowerRangeCeiling = midpoint;
+                int upperRangeFloor = midpoint + 1;
+                int upperRangeCeiling = ceiling;
+
+                // Count number of items in lower range
+                int itemsInLowerRange = numbers.Count(item => item >= lowerRangeFloor && item <= lowerRangeCeiling);
+
+                int distinctPossibleIntegersInLowerRange = lowerRangeCeiling - lowerRangeFloor + 1;
+
+                if (itemsInLowerRange > distinctPossibleIntegersInLowerRange)
+                {
+                    // There must be a duplicate in the lower range
+                    // so use the same approach iteratively on that range
+                    floor = lowerRangeFloor;
+                    ceiling = lowerRangeCeiling;
+                }
+                else
+                {
+                    // There must be a duplicate in the upper range
+                    // so use the same approach iteratively on that range
+                    floor = upperRangeFloor;
+                    ceiling = upperRangeCeiling;
+                }
+            }
+
+            // Floor and ceiling have converged
+            // We found a number that repeats!
+            return floor;
+        }
+    }
+}
---
+++ Assets/Scripts/Tests/ArraysAndStrings/FindDupsTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+namespace InterviewQuestions.ArraysAndStrings.Tests
+{
+    public class FindDupsTests
+    {
+        [Test]
+        public void JustTheRepeatedNumberTest()
+        {
+            var numbers = new int[] { 1, 1 };
+            var expected = 1;
+            var actual = ArrayExtensions.FindRepeat(numbers);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ShortArrayTest()
+        {
+            var numbers = new int[] { 1, 2, 3, 2 };
+            var expected = 2;
+            var actual = ArrayExtensions.FindRepeat(numbers);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void MediumArrayTest()
+        {
+            var numbers = new int[] { 1, 2, 5, 5, 5, 5 };
+            var expected = 5;
+            var actual = ArrayExtensions.FindRepeat(numbers);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void LongArrayTest()
+        {
+            var numbers = new int[] { 4, 1, 4, 8, 3, 2, 7, 6, 5 };
+            var expected = 4;
+            var actual = ArrayExtensions.FindRepeat(numbers);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
