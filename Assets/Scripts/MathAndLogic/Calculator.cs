a1cecd87c3860da003259ac03903bfb8b0d88d04
Joseph Leybovich <joseph.leybovich@gmail.com>
2020-05-13 17:27:55 -0700

    calculator

---
+++ Assets/Scripts/MathAndLogic/Calculator.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewQuestions.MathAndLogic
+{
+    public static partial class MathExtensions
+    {
+        public static int Calculate(string equation)
+        {
+            if (string.IsNullOrEmpty(equation)) return 0;
+            int length = equation.Length;
+
+            Stack<int> resultStack = new Stack<int>();
+
+            int num = 0;
+            char sign = '+';
+            for (int i = 0; i < length; ++i)
+            {
+                char val = equation[i];
+
+                if (char.IsDigit(val))
+                {
+                    num = num * 10 + val - '0';
+                }
+
+                if (!char.IsDigit(val) || i == length - 1)
+                {
+                    switch (sign)
+                    {
+                        case '-':
+                            resultStack.Push(-num);
+                            break;
+                        case '+':
+                            resultStack.Push(num);
+                            break;
+                        case '*':
+                            resultStack.Push(resultStack.Pop() * num);
+                            break;
+                        case '/':
+                            resultStack.Push(resultStack.Pop() / num);
+                            break;
+                        default:
+                            break;
+                    }
+
+                    sign = val;
+                    num = 0;
+                }
+            }
+
+            int total = 0;
+            while(resultStack.Count > 0)
+            {
+                total += resultStack.Pop();
+            }
+
+            return total;
+        }
+    }
+}
---
+++ Assets/Scripts/Tests/MathAndLogic/CalculatorTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+
+namespace InterviewQuestions.MathAndLogic.Tests
+{
+    public class CalculatorTests
+    {
+        [Test]
+        public void NullInputTest()
+        {
+            var expected = 0;
+            var actual = MathExtensions.Calculate(null);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ShortInputTest()
+        {
+            var expected = 3;
+            var actual = MathExtensions.Calculate("1+2");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SubtractionoTest()
+        {
+            var expected = -1;
+            var actual = MathExtensions.Calculate("1+2-4");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void MultiplicationTest()
+        {
+            var expected = 30;
+            var actual = MathExtensions.Calculate("2*3*5");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void DivisionTest()
+        {
+            var expected = 2;
+            var actual = MathExtensions.Calculate("30/5/3");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void AllOperationsTest()
+        {
+            var expected = -1;
+            var actual = MathExtensions.Calculate("1+2-4*5/5");
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
