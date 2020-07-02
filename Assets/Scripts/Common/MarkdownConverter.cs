ffca9f5ba427a7083edfaaf0994a69421af58182
Joseph Leybovich <joseph.leybovich@gmail.com>
2020-07-02 03:20:37 -0700

    markdown converter start

---
+++ Assets/Scripts/Common/MarkdownConverter.cs
@@ -0,0 +1,72 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+public class MarkdownConverter
+{
+    private const int MinHeader = 1;
+    private const int MaxHeader = 6;
+
+    public void MarkdownToHtml(
+        string inputMarkdownFilePath,
+        string outputMarkdownFilePath)
+    {
+        string input = File.ReadAllText(inputMarkdownFilePath);
+        string converted = Convert(input);
+        File.WriteAllText(outputMarkdownFilePath, converted);
+    }
+
+    private string Convert(string input)
+    {
+        string converted = input;
+        converted = ConvertHeaders(ref converted);
+        converted = ConvertFormat(ref converted, 2, "strong");
+        converted = ConvertFormat(ref converted, 1, "em");
+        return converted;
+    }
+
+    private string ConvertHeaders(ref string input)
+    {
+        for(int header = MaxHeader; header >= MinHeader; --header)
+        {
+            var pattern = @"^#{" + header + "}.*";
+            var matches = Regex.Matches(input, pattern, RegexOptions.Multiline);
+
+            foreach(Match match in matches)
+            {
+                string converted = Regex.Replace(match.ToString(),
+                    @"^#+\s*",
+                    "<h" + header + ">");
+                converted = Regex.Replace(converted,
+                    @"\s*#*$",
+                    "");
+                converted += "</h" + header + ">";
+
+                input = input.Replace(match.ToString(), converted);
+            }
+        }
+
+        return input;
+    }
+
+    private string ConvertFormat(ref string input, int num, string tag)
+    {
+        var pattern = "_{" + num + "}.*" + "_{" + num + @"}|\*{" + num + @"}.*\*{"
+            + num + "}";
+        var matches = Regex.Matches(input, pattern, RegexOptions.Multiline);
+
+        foreach (Match match in matches)
+        {
+            string converted = Regex.Replace(match.ToString(),
+                @"^(_{" + num + @"}|\*{" + num + "})",
+                @"<" + tag + ">");
+            converted = Regex.Replace(converted,
+                @"(_{" + num + @"}|\*{" + num + "})$",
+                @"</" + tag + ">");
+
+            input = input.Replace(match.ToString(), converted);
+        }
+
+        return input;
+    }
+}
---
+++ Assets/Scripts/Tests/Common/MarkdownConverterTests.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using NUnit.Framework;
+using UnityEngine;
+
+public class MarkdownConverterTests
+{
+    private const string DataDirectory = "/Users/joe/git/InterviewTestQuestionsLab/Assets/Data/";
+    private const string ExpectedPath = DataDirectory + "Expected.html";
+    private const string InputPath = DataDirectory + "Input.md";
+    private const string OutputPath = DataDirectory + "Output.html";
+
+    [Test]
+    public static void ExampleDriver()
+    {
+        var converter = new MarkdownConverter();
+        var expected = File.ReadAllText(ExpectedPath);
+        converter.MarkdownToHtml(InputPath, OutputPath);
+        var actual = File.ReadAllText(OutputPath);
+        Debug.Log(actual);
+        Assert.AreEqual(expected, actual);
+    }
+}
